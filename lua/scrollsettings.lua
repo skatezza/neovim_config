0f6622312b66d5144c263691578a91e46cea4f15
@skatezza <genghiscans@gmail.com>
2023-10-17 01:31:44 +0200

    updates

--- lua/scrollsettings.lua
+++ lua/scrollsettings.lua
@@ -2,5 +2,3 @@ vim.api.nvim_set_keymap('n', '{', ':call smooth_scroll#up(&scroll*2, 4, 3)<cr>',
 vim.api.nvim_set_keymap('n', '}', ':call smooth_scroll#down(&scroll*2, 4, 3)<cr>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-u>', ':call smooth_scroll#up(&scroll, 4, 2)<cr>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-d>', ':call smooth_scroll#down(&scroll, 4, 2)<cr>', {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<C-k>', ':call smooth_scroll#up(&scroll, 4, 2)<cr>', {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<C-j>', ':call smooth_scroll#down(&scroll, 4, 2)<cr>', {noremap = true, silent = true})
